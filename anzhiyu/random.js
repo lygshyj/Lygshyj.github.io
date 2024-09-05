e5e5ee71a2b0157384c3ff6132e229d124c4c4ad
Lygshyj <2549039841@qq.com>
2024-09-05 15:49:21 +0800

    Site updated: 2024-09-05 15:49:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/02/hello-world/"];function toRandomPost(){
+var posts=["2024/09/02/hello-world/","2024/09/04/Hexo学习之路-命令篇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
